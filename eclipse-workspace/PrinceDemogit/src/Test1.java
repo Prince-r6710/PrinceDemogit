562bc7ce821b008faa1627a1af53ed624e1b89f0
Prince Kumar <prince.kumar.1@maersk.com>
2022-03-03 18:12:22 +0530

    first comm...

--- eclipse-workspace/PrinceDemogit/src/Test1.java
+++ eclipse-workspace/PrinceDemogit/src/Test1.java
@@ -1,15 +1,16 @@
 
-public class Test {
+public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
 		int b=20;
-		if(a>b) {
-			System.out.println(a+b);
+		if(b>a)
+		{
+			System.out.println(b-a);
 		}
 		else
-			System.out.println(b-a);
+			System.out.println(a-b);
 
 	}
 
